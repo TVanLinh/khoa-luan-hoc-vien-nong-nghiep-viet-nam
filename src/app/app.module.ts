4cb9eb5aedef3756e43a565703dfc5f035587e1c
linhtran <linhtran180895@gmail.com>
2017-07-22 16:17:56 +0700

    clear frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SlidersComponent } from './sliders/sliders.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { PersonsComponent } from './persons/persons.component';
+import { BannerComponent } from './banner/banner.component';
+import { SliderBannerComponent } from './sliders/slider-banner/slider-banner.component';
+import { ComponentsComponent } from './components/components.component';
+import { NewComponent } from './components/new/new.component';
+import { TabFacultyComponent } from './tabs/tab-faculty/tab-faculty.component';
+import { SliderFacultyComponent } from './sliders/slider-faculty/slider-faculty.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { PublishComponent } from './components/publish/publish.component';
+import { TabLeftComponent } from './tabs/tab-left/tab-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    SlidersComponent,
+    TabsComponent,
+    PersonsComponent,
+    BannerComponent,
+    SliderBannerComponent,
+    ComponentsComponent,
+    NewComponent,
+    TabFacultyComponent,
+    SliderFacultyComponent,
+    CalendarComponent,
+    PublishComponent,
+    TabLeftComponent
   ],
   imports: [
     BrowserModule
