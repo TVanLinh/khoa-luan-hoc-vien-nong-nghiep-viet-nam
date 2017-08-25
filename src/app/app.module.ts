93a9dbc2ab0182e2d6933fdf306d022ee09f62a4
linhtran <linhtran180895@gmail.com>
2017-08-25 23:50:39 +0700

    create commponent curricumlun-vitae.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SlidersComponent } from './sliders/sliders.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { PersonsComponent } from './persons/persons.component';
-import { BannerComponent } from './banner/banner.component';
-import { SliderBannerComponent } from './sliders/slider-banner/slider-banner.component';
+import { HeaderComponent } from './components/headers/header/header.component';
+import { FooterComponent } from './components/footers/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SlidersComponent } from './components/sliders/sliders.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { PersonsComponent } from './components/persons/persons.component';
+import { BannerComponent } from './components/banners/banner/banner.component';
+import { SliderBannerComponent } from './components/sliders/slider-banner/slider-banner.component';
 import { ComponentsComponent } from './components/components.component';
 import { NewComponent } from './components/new/new.component';
-import { TabFacultyComponent } from './tabs/tab-faculty/tab-faculty.component';
-import { SliderFacultyComponent } from './sliders/slider-faculty/slider-faculty.component';
+import { TabFacultyComponent } from './components/tabs/tab-faculty/tab-faculty.component';
+import { SliderFacultyComponent } from './components/sliders/slider-faculty/slider-faculty.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { PublishComponent } from './components/publish/publish.component';
-import { TabLeftComponent } from './tabs/tab-left/tab-left.component';
+import { TabLeftComponent } from './components/tabs/tab-left/tab-left.component';
 import { TopComponent } from './components/top/top.component';
 import {ToggleClass} from "app/core/directives/toogle.class.directive";
 import { MapComponent } from './components/map/map.component';
 import {AgmCoreModule} from "@agm/core";
+import { CurriculumVitaeComponent } from './personnels/curriculum-vitae/curriculum-vitae.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -43,13 +45,16 @@ import {AgmCoreModule} from "@agm/core";
     TopComponent,
     ToggleClass,
     MapComponent,
+    CurriculumVitaeComponent,
 
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB9s64voWTBx5s4qadlcWwbCNhWJU5Sje0'
-    })
+    }),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/persons/persons.component.ts
+++ src/app/components/persons/persons.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: '[app-persons]',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonsComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+
 
   ngOnInit() {
   }
